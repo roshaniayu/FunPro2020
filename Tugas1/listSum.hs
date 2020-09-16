97a3e4b535378cd85218b1db3c191dcef666505e
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2020-09-16 21:46:35 +0700

    add listSum and listSumArea

---
+++ Tugas1/listSum.hs
@@ -0,0 +1,4 @@
+-- menerima parameter List Integer
+listSum :: [Integer] -> Integer
+-- mengembalikan total dari elemen didalam list tersebut
+listSum a = sum a
---
+++ Tugas1/listSumArea.hs
@@ -0,0 +1,7 @@
+circleArea :: Double -> Double
+circleArea r = 3.14 * r ^ 2
+
+-- menerima parameter List circleArea
+listSumArea :: [Double] -> Double
+-- mengembalikan total dari semua area di dalam list tersebut
+listSumArea a = sum a
