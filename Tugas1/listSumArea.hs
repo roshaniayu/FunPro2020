69eefc53585064a6234b5b1d9b940507ee64a4a3
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2020-09-16 22:41:56 +0700

    modify listSumArea implementation

--- Tugas1/listSumArea.hs
+++ Tugas1/listSumArea.hs
@@ -2,7 +2,7 @@ circleArea :: Double -> Double
 circleArea r = 3.14 * r ^ 2
 
 -- menerima parameter List circleArea
-listSumArea :: [circleArea a] -> Double
+listSumArea :: [Double] -> Double
 -- mengembalikan total dari semua area di dalam list tersebut
 listSumArea [] = 0
 listSumArea (x : xs) = x + listSumArea xs
